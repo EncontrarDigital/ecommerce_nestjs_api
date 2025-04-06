d5724e42d46023b7215564fb0a0d5e91ba7983e0
Grécio Diogo <fonebahia8@gmail.com>
2025-04-06 23:52:50 +0100

    fix: define postgres default port

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -3,8 +3,8 @@ export default () => ({
   postgres: {
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
-    username: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
+    username: 'postgres',
+    password: 'postgres',
     database: process.env.POSTGRES_DB,
   },
   session: {
