597993a4f9f5a12d8a9dbcb570743d2296b7dc04
Grécio Diogo <fonebahia8@gmail.com>
2025-04-14 16:44:21 +0100

    fix: add login to request type

--- src/@types/express/index.d.ts
+++ src/@types/express/index.d.ts
@@ -4,6 +4,7 @@ import { Request } from 'express';
 declare module 'express-serve-static-core' {
   interface Request {
     user?: User;
+    login(user: any, done: (err: any) => void): void;
     logOut?: (callback: (err?: any) => void) => void;
   }
 }
